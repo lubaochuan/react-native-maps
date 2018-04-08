91d6f16d2550d71dbad7b08f5d5ba7133dcf8c29
lubaochuan <lubaochuan@gmail.com>
2018-04-08 04:20:14 -0500

    add support for region change

--- App.js
+++ App.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { MapView } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  };
+
+  onRegionChange = (region) => {
+    this.setState({ region });
+  }
+
   render() {
     return (
+      
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
       />
     );
   }
--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # Example app for experimenting with MapView
 
-Install libraries:
-```
-npm install react-native-maps --save
-```
-
 Try this app:
 ```
 npm install
@@ -12,3 +7,5 @@ npm install
 Open this app with an expo client.
 
 [Expo MapView Component](https://docs.expo.io/versions/v26.0.0/sdk/map-view)
+
+[Component API](https://github.com/react-community/react-native-maps)
