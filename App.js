531d0a466b1786efc730cf2f0deeb4e9229c952c
lubaochuan <lubaochuan@gmail.com>
2018-04-08 04:35:28 -0500

    add marker array

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import { MapView } from 'expo';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
+function createMarker(modifier = 1) {
+  return {
+    latitude: LATITUDE - (SPACE * modifier),
+    longitude: LONGITUDE - (SPACE * modifier),
+    title: "title for "+modifier,
+    description: "description for "+modifier,
+  };
+}
+
+const MARKERS = [
+  createMarker(),
+  createMarker(2),
+  createMarker(3),
+  createMarker(4),
+];
 
 export default class App extends React.Component {
   state = {
     region: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
     },
+    markers: MARKERS,
   };
 
   onRegionChange = (region) => {
@@ -17,12 +44,19 @@ export default class App extends React.Component {
 
   render() {
     return (
-      
       <MapView
         style={{ flex: 1 }}
         region={this.state.region}
-        onRegionChange={this.onRegionChange}
-      />
+        onRegionChange={this.onRegionChange}>
+        {this.state.markers.map((marker, i) => (
+           <MapView.Marker
+             key={i}
+             coordinate={marker}
+             title={marker.title}
+             description={marker.description}
+           />
+         ))}
+      </MapView>
     );
   }
 }
